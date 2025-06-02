a6b707af91a5027cf71408af90ffea8a36cc2759
ThiagoSaraceni <thiagorx123@gmail.com>
2025-06-01 23:04:16 -0300

    feat: fechando submenu no botao voltar, e melhorando footer

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const menuButton = document.getElementById("hamburguer-icon");
 const mobileMenu = document.getElementById("mobile-menu");
+const btnBack = document.getElementById("btn-back");
 
 menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+});
+
+btnBack.addEventListener("click", () => {
   mobileMenu.classList.toggle("open");
 });
